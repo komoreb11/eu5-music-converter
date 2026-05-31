d100255601cc4fb5997be989af28788655bb2220
komoreb11 <ayutukov@proton.me>
2026-05-31 15:51:12 +0200

    Add cache-busting timestamp to curl URL

--- launch.cmd
+++ launch.cmd
@@ -1,4 +1,5 @@
 @echo off
-curl -sL -o "%TEMP%\eu4snd.ps1" https://raw.githubusercontent.com/komoreb11/eu5-music-converter/main/EU4_Soundtrack_Setup.ps1
+for /f %%i in ('powershell -Command "Get-Date -UFormat %%s"') do set TS=%%i
+curl -sL -o "%TEMP%\eu4snd.ps1" "https://raw.githubusercontent.com/komoreb11/eu5-music-converter/main/EU4_Soundtrack_Setup.ps1?t=%TS%"
 powershell -NoProfile -ExecutionPolicy Bypass -File "%TEMP%\eu4snd.ps1"
 %*
