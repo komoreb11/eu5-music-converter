240cbcdc4a247b62a38813513a363c405e227d3e
komoreb11 <ayutukov@proton.me>
2026-05-31 15:52:30 +0200

    Use start to launch game non-blocking so cmd window closes

--- launch.cmd
+++ launch.cmd
@@ -2,4 +2,4 @@
 for /f %%i in ('powershell -Command "Get-Date -UFormat %%s"') do set TS=%%i
 curl -sL -o "%TEMP%\eu4snd.ps1" "https://raw.githubusercontent.com/komoreb11/eu5-music-converter/main/EU4_Soundtrack_Setup.ps1?t=%TS%"
 powershell -NoProfile -ExecutionPolicy Bypass -File "%TEMP%\eu4snd.ps1"
-%*
+start "" %*
